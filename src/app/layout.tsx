b34d2ffd329a46a6b00c1bec4ecb731f7f8d1b32
ronitrajfr <ronitrajofficial7@gmail.com>
2025-04-01 01:39:13 +0530

    feat: auth implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-
+import Providers from "@/components/Provider";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
@@ -19,7 +19,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
+      <Providers>
+        <body>{children}</body>
+      </Providers>
     </html>
   );
 }
