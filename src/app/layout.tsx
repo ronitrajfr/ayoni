ed3a16758f8c4f6940793a05b1a72ee6d81608f0
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-23 20:43:11 +0600

    added toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 import Script from "next/script";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Ayoni - Home",
@@ -41,6 +42,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <Toaster richColors position="top-right" /> 
             {children}
           </ThemeProvider>
         </body>
