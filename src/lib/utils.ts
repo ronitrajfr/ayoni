6bdb59c0ea643d1a089510d446e69f9033b378cd
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-23 20:41:34 +0600

    added util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getHostFromUrl = (url: string) => {
+  if (!url || url === "direct") return "Direct";
+  try {
+    const urlWithProtocol = url.startsWith("http") ? url : `http://${url}`;
+    return new URL(urlWithProtocol).host;
+  } catch {
+    return url;
+  }
+};
+
+export const getSimplePath = (url: string) => {
+  try {
+    return new URL(url).pathname || url;
+  } catch {
+    return url;
+  }
+};
